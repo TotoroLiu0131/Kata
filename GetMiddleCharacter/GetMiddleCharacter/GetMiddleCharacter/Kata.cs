02d92c42cc48bfa1b045faa54243d308a15799d0
Benjamin Liu Po-Ching <benjamin.liu@titansoft.com.sg>
2018-07-28 23:41:11 +0800

    origin

---
+++ GetMiddleCharacter/GetMiddleCharacter/GetMiddleCharacter/Kata.cs
@@ -0,0 +1,11 @@
+﻿namespace GetMiddleCharacter
+{
+    public class Kata
+    {
+        public static string GetMiddleCharacter(string input)
+        {
+            var middleIndex = input.Length / 2;
+            return input.Length % 2 == 0 ? input.Substring(middleIndex - 1, 2) : input[middleIndex].ToString();
+        }
+    }
+}
---
+++ GetMiddleCharacter/GetMiddleCharacter/GetMiddleCharacterTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using GetMiddleCharacter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GetMiddleCharacter.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [TestCase("test", "es",Description = "test_result_should_be_es")]
+        [TestCase("testing", "t", Description = "testing_result_should_be_t")]
+        [TestCase("middle", "dd", Description = "middle_result_should_be_dd")]
+        [TestCase("A", "A",Description = "A_result_should_be_A" )]
+        public void GetMiddleCharacterTest(string input, string expect)
+        {
+            Assert.AreEqual(expect, Kata.GetMiddleCharacter(input));
+        }
+    }
+}
