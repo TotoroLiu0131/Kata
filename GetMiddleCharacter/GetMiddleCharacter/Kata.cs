fe0a3244f30dfa97d19320d860067d690c43030c
Benjamin Liu Po-Ching <benjamin.liu@titansoft.com.sg>
2018-09-04 23:11:46 +0800

    init GetMiddleCharacter

---
+++ GetMiddleCharacter/GetMiddleCharacter/Kata.cs
@@ -0,0 +1,12 @@
+﻿namespace GetMiddleCharacter
+{
+    public class Kata
+    {
+        public string GetMiddleCharacter(string input)
+        {
+            return input.Length % 2 == 0 ?
+                input.Substring(input.Length / 2 - 1, 2) :
+                input.Substring(input.Length / 2, 1);
+        }
+    }
+}
---
+++ GetMiddleCharacter/GetMiddleCharacterTests/KataTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace GetMiddleCharacter.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [TestCase("test", ExpectedResult = "es", TestName = "input_test_should_return_es")]
+        [TestCase("testing", ExpectedResult = "t", TestName = "input_testing_should_return_t")]
+        [TestCase("A", ExpectedResult = "A", TestName = "input_A_should_return_A")]
+        public string GetMiddleCharacterTest(string input)
+        {
+            return new Kata().GetMiddleCharacter(input);
+        }
+    }
+}
