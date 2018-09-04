698c38324dc2b43c863569f335748d3df3081366
Benjamin Liu Po-Ching <benjamin.liu@titansoft.com.sg>
2018-09-04 23:51:40 +0800

    init PersistentBugger

---
+++ PersistentBugger/PersistentBugger/Kata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PersistentBugger
+{
+    public class Kata
+    {
+        public long GetPersistentBugger(long input)
+        {
+            var arr = IntToCharArray(input);
+            var count = 0;
+            while (arr.Length > 1)
+            {
+                count++;
+                arr = IntToCharArray(ReduceArray(arr));
+            }
+            return count;
+        }
+
+        private static int ReduceArray(char[] arr)
+        {
+            var i = 1;
+            foreach (var item in arr)
+            {
+                i *= (int)Char.GetNumericValue(item);
+            }
+
+            return i;
+        }
+
+        private static char[] IntToCharArray(long input)
+        {
+            return input.ToString().ToCharArray();
+        }
+    }
+}
---
+++ PersistentBugger/PersistentBuggerTests/KataTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace PersistentBugger.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [TestCase(39, ExpectedResult = 3, TestName = "input_39_result_should_be_3")]
+        [TestCase(999, ExpectedResult = 4, TestName = "input_999_result_should_be_4")]
+        [TestCase(4, ExpectedResult = 0, TestName = "input_4_result_should_be_0")]
+        public long GetPersistentBuggerTest(long input)
+        {
+            return new Kata().GetPersistentBugger(input);
+        }
+    }
+}
